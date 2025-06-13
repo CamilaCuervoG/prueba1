9cb37ad9319ed46e458dbd3563d2662f6d78bb7f
valenF15 <valen.lopez.15@hotmail.com>
2025-06-13 01:04:44 -0500

    Cambios Valentina y camilo finalizados

---
+++ financiacion.js
@@ -0,0 +1,39 @@
+// Activar le botón que lleva al evento Secretaría de Desarrollo Económico
+document.getElementById("enlace1").addEventListener("click", function () {
+    window.open("https://www.medellin.gov.co/es/secretaria-desarrollo-economico/subsecretaria-de-creacion-y-fortalecimiento-empresarial/centro-de-emprendimiento-y-empleo/", "_blank");
+    }); 
+
+// Activar le botón que lleva al evento Parque del Emprendimiento
+document.getElementById("enlace2").addEventListener("click", function () {
+    window.open("https://parquedelemprendimiento.co", "_blank");
+    }); 
+
+// Activar le botón que lleva al evento Créame
+document.getElementById("enlace3").addEventListener("click", function () {
+    window.open("https://www.creame.com.co", "_blank");
+    }); 
+
+// Activar le botón que lleva al evento Innóvate Grupo EPM
+document.getElementById("enlace4").addEventListener("click", function () {
+    window.open("https://www.epm.com.co/institucional/innovacion/innovate-epm/categoria-emprendiendo/", "_blank");
+    }); 
+
+    // Activar le botón que lleva al evento Canoa Impact Flows
+document.getElementById("enlace5").addEventListener("click", function () {
+    window.open("https://www.canoaimpacto.com", "_blank");
+    }); 
+
+    // Activar le botón que lleva al evento Fondo Mujer
+document.getElementById("enlace6").addEventListener("click", function () {
+    window.open("https://fondomujer.gov.co/portfolio/emprende-pro-mujer/", "_blank");
+    }); 
+
+    // Activar le botón que lleva al evento Centro de Servicios Compartidos - Ruta N
+document.getElementById("enlace7").addEventListener("click", function () {
+    window.open("https://rutanmedellin.org/programas/ofertas/centro-de-servicios-compartidos?hsLang=es", "_blank");
+    }); 
+
+    // Activar le botón que lleva al evento Fondo Mujer
+document.getElementById("enlace8").addEventListener("click", function () {
+    window.open("https://www.rutadelemprendimientomedellin.com", "_blank");
+    }); 
